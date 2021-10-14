822d524804175187b53d27f4aca05a5dcfabeacd
banan <edem0004@edu.easj.dk>
2021-10-14 22:59:46 +0200

    Assignment 2

---
+++ RESTService/Controllers/BooksController.cs
@@ -0,0 +1,57 @@
+﻿using Library;
+using Microsoft.AspNetCore.Mvc;
+using RESTService.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RESTService.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+
+        private readonly BooksManager _manager = new BooksManager();
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        public List<Book> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        public Book Get(string id)
+        {
+            return _manager.GetBook(id);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public void Post([FromBody] Book book)
+        {
+            _manager.Add(book);
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] Book book)
+        {
+            _manager.Update(id, book);
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            _manager.Delete(id);
+        }
+    }
+}
---
+++ RESTService/Managers/BooksManager.cs
@@ -0,0 +1,48 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTService.Managers
+{
+    public class BooksManager
+    {
+        public static List<Book> list = new List<Book>
+        {
+            new Book {ISBN13 = "111", Author = "Person1", Title = "Title1", PageNumber = 11},
+            new Book {ISBN13 = "222", Author = "Person2", Title = "Title2", PageNumber = 22},
+            new Book {ISBN13 = "333", Author = "Person3", Title = "Title3", PageNumber = 33},
+        };
+
+        public List<Book> GetAll()
+        {
+            return new List<Book>(list);
+        }
+
+        public Book GetBook(string ISBN13)
+        {
+            return list.Find(book => book.ISBN13.Contains(ISBN13));
+        }
+
+        public Book Add(Book newBook)
+        {
+            list.Add(newBook);
+            return newBook;
+        }
+
+        public void Delete(string ISBN13)
+        {
+            var  index = list.FindIndex(book => book.ISBN13 == ISBN13);
+            if (index != -1)
+                list.RemoveAt(index);
+        }
+
+        public void Update(string ISBN13, Book book)
+        {
+            var index = list.FindIndex(book => book.ISBN13 == ISBN13);
+            if (index != -1)
+                list[index] = book;
+        }
+    }
+}
